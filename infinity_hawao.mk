c77c7cbf22abdf37b3aac38cccd9356a263ef607
thecapibara <49768197+thecapibara@users.noreply.github.com>
2025-06-10 09:28:49 +0300

    Update infinity_hawao.mk
    Skipped blur option due to 4GB RAM only

--- infinity_hawao.mk
+++ infinity_hawao.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 INFINITY_BUILD_TYPE := UNOFFICIAL
 INFINITY_MAINTAINER := "JustGL"
 TARGET_SUPPORTS_BLUR := false
-WITH_GAPPS := true
+WITH_GAPPS := false
 TARGET_SHIPS_FULL_GAPPS := false
 TARGET_SHIPS_GOOGLE_DIALER := false
 USE_MOTO_CALCULATOR := true
